6220ecf70310af39b40c5ed6c4a45793d8165aa3
Falko Gebel <63905869+FalkoGebel@users.noreply.github.com>
2024-02-15 23:11:35 +0100

    added library project and library test project and started logic implementation

---
+++ VisualizerLibrary/VisualizerLogic.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace VisualizerLibrary
+{
+    public static class VisualizerLogic
+    {
+        public static void ConnectToNavDatabase(string server, string database)
+        {
+            if (server == string.Empty)
+                throw new ArgumentException(Properties.Resources.EXP_SERVER_MISSING);
+
+            if (database == string.Empty)
+                throw new ArgumentException(Properties.Resources.EXP_DATABASE_MISSING);
+
+            string connectionString = $@"Data Source={server};Initial Catalog={database};Integrated Security=SSPI;";
+
+            SqlConnection cnn = new(connectionString);
+            cnn.Open();
+            cnn.Close();
+        }
+    }
+}
